b6b1f6a768f41f29dff9b04391e9c15ab887e6e1
Redemple3 <marceloredemple332@gmail.com>
2021-10-28 18:42:09 +0800

    fix

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('ROOT_URL', 'https://fathomless-lake-84500.herokuapp.com/');
-	define('DB_HOST', 'http://sql6.freesqldatabase.com/');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
 	define('DB_USER', 'sql6447363');
 	define('DB_PASS', 'H3gsmzD6Ir');
 	define('DB_NAME', 'sql6447363');
